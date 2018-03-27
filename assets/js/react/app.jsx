c17cc964fd5a791e01c641ef319c814d88dbad04
MichałPietrasz <michal.pietrasz@gmail.com>
2018-03-27 21:46:18 +0200

    Simplify store app persistor

--- assets/js/react/app.jsx
+++ assets/js/react/app.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import Main from './components/Layout'
-import createAppStore from './store/storage'
+import store from './store/index'
 
-const { persistor, store } = createAppStore()
+import createAppStore from './store/storage'
+const history = createBrowserHistory()
+//const { persistor, store } = createAppStore()
 
 ReactDOM.render((
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </PersistGate>
+    <Router history={history}>
+      <Main/>
+    </Router>
   </Provider>
 ), document.getElementById('root'))
