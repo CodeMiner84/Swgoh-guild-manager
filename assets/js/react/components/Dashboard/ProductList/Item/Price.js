f33d6a31cea0d01a7f101301592ab201f5fde21a
MichałPietrasz <michal.pietrasz@gmail.com>
2018-03-28 17:58:03 +0200

    Change add mod button text

--- assets/js/react/components/Dashboard/ProductList/Item/Price.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-    color: #FF0000;
-    font-weight: bold;
-    text-transform: italic;
-`
--- assets/js/react/components/Dashboard/ProductList/Item/Title.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-    font-weight: bold;
-    height: 24px;
-    line-height: 24px;
-    overflow: hidden;
-    text-overflow: elipsis;
-`
