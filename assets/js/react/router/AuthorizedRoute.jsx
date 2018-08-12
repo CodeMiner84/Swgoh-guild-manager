92f33e06be26652f59eaa4001ce869c0eb23b7db
MichałPietrasz <michal.pietrasz@gmail.com>
2018-08-12 09:58:36 +0200

    Modify authorize route. Fetch data in HOC

--- assets/js/react/router/AuthorizedRoute.jsx
+++ assets/js/react/router/AuthorizedRoute.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Route, Redirect, Link } from 'react-router-dom'
 import { createSelector } from 'reselect'
 import { connect } from 'react-redux'
 import { isAuth } from '../utils/auth'
-import actions from '../actions/mods'
-import ReactTimeout from 'react-timeout'
+import accountActions from '../actions/account'
+
+class AuthHOC extends Component {
+  componentDidMount() {
+    this.props.getAccount()
+  }
+  render() {
+    const { component: Component, searchPhrase: phrase, ...rest } = this.props
+    return (
+      <Route
+        {...rest}
+        render={props => isAuth() ? (
+          (
+            rest.auth.uuid || rest.noUuid !== undefined ?
+              <Component phrase={phrase} {...props} /> :
+              <div className="alert alert-danger">
+                You need to map your user uuid <Link to={'/account'}>HERE</Link>
+              </div>
+          )
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location },
+            }}
+          />
+        )}
+      />
+    )
+  }
+}
 
 const AuthorizedRoute = ({ component: Component, searchPhrase: phrase, ...rest }) => {
+
+  console.log('estsetsets');
+  console.log('rest', rest);
 
   return (
     <Route
@@ -40,18 +72,14 @@ AuthorizedRoute.propTypes = {
   searchPhrase: PropTypes.string,
 }
 
-
-const getAccount = () => state => state.account.auth
-
-const authSelector = createSelector(
-  [getAccount()],
-  auth => auth,
-)
-
 function mapStateToProps(state) {
   return {
-    auth: authSelector(state),
+    auth: state.account.auth,
   }
 }
 
-export default connect(mapStateToProps, null)(AuthorizedRoute)
+const mapDispatchToProps = {
+  getAccount: accountActions.getAccount
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthHOC)
