706f617fca3c4f67d6be2101301afc8bcd1480b5
MichałPietrasz <michal.pietrasz@gmail.com>
2018-08-16 23:23:01 +0200

    revert paypal donation

--- assets/js/react/components/Layout/navbar.jsx
+++ assets/js/react/components/Layout/navbar.jsx
@@ -50,6 +50,15 @@ const NavbarMenu = () => (
         </NavItem>
       </Nav>
     </Navbar.Collapse>
+    <div>
+      <hr/>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="YFN6J65X8T74W" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/PL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+        <img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+    </div>
   </Navbar>
 )
 
